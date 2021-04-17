45e5e061de2b3e2ea83cc3c384e0d196f4b4c3c3
uchidanihachi28 <uchida@nihachi.com>
2021-04-17 17:59:49 +0900

    showtimepickerを表示させようとしている（途中）

---
+++ flutter_app2/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyAppState());
+
+class MyAppState extends State<MyApp> {
+  var _value = '';
+
+  Future _select()async{
+    TimeOfDay selected = await showTimePicker(context: context, initialTime: TimeOfDay.now());
+  if (selected != null) {
+    setState(() => _value = selected.toString());
+    }
+  }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('showDatePicker')),
+      body:  Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(_value),
+            RaisedButton
+              (onPressed: _select, child: const Text('開く'),
+            )
+          ],
+        ),
+      ),
+    ),
+    );
+  }
+}
+
